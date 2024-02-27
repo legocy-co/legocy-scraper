f5ae8784392379ba77cc121492f8bcb4587eb321
Tikhon Kozlov <STPKBFF@yandex.ru>
2024-02-27 17:53:48 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from load.add_to_legocy import add_to_legocy
-from transform.sets_list_filtered import download_photos
-from extract.sets_list_raw import retrieve_links
+from load.add_to_legocy import add_to_legocy as load
+from transform.sets_list_filtered import filter_sets as transform
+from extract.sets_list_raw import retrieve_links as extract
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
@@ -8,6 +8,11 @@ headers = {
 }
 
 if __name__ == '__main__':
-    list_of_sets = retrieve_links(headers)
-    set_information = download_photos(list_of_sets, headers)
-    add_to_legocy(set_information)
+    load(
+        transform(
+            extract(
+                headers
+            ),
+            headers
+        )
+    )
